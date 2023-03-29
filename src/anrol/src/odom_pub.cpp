28911bc9a05eaee4df1258ff5a774ef43eff8103
YashAgra <yash20551@iiitd.ac.in>
2023-03-29 06:41:46 +0530

    updated odometry params

--- src/anrol/src/odom_pub.cpp
+++ src/anrol/src/odom_pub.cpp
@@ -31,6 +31,7 @@
 #include <geometry_msgs/PoseStamped.h>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2_ros/transform_broadcaster.h>
+#include <tf/transform_broadcaster.h>
 #include <cmath>
  
 // Create odometry data publishers
@@ -38,7 +39,7 @@ ros::Publisher odom_data_pub;
 ros::Publisher odom_data_pub_quat;
 nav_msgs::Odometry odomNew;
 nav_msgs::Odometry odomOld;
- 
+
 // Initial pose
 const double initialX = 0.0;
 const double initialY = 0.0;
@@ -46,10 +47,10 @@ const double initialTheta = 0.00000000001;
 const double PI = 3.141592;
  
 // Robot physical constants
-const double TICKS_PER_REVOLUTION = 4332; // For reference purposes.
+const double TICKS_PER_REVOLUTION = 13000; // For reference purposes.
 const double WHEEL_RADIUS = 0.055; // Wheel radius in meters
-const double WHEEL_BASE = 0.34; // Center of left tire to center of right tire
-const double TICKS_PER_METER = 14000; // Original was 2800
+const double WHEEL_BASE = 0.33; // Center of left tire to center of right tire
+
  
 // Distance both wheels have traveled
 double distanceLeft = 0;
@@ -111,9 +112,8 @@ void Calc_Right(const std_msgs::Int16& rightCount) {
  
 // Publish a nav_msgs::Odometry message in quaternion format
 void publish_quat() {
- 
-  tf2::Quaternion q;
-         
+  
+  tf2::Quaternion q;       
   q.setRPY(0, 0, odomNew.pose.pose.orientation.z);
  
   nav_msgs::Odometry quatOdom;
@@ -231,18 +231,19 @@ int main(int argc, char **argv) {
   ros::Subscriber subForRightCounts = node.subscribe("right_ticks", 100, Calc_Right, ros::TransportHints().tcpNoDelay());
   ros::Subscriber subForLeftCounts = node.subscribe("left_ticks", 100, Calc_Left, ros::TransportHints().tcpNoDelay());
   ros::Subscriber subInitialPose = node.subscribe("initial_2d", 1, set_initial_2d);
- 
+  
   // Publisher of simple odom message where orientation.z is an euler angle
   odom_data_pub = node.advertise<nav_msgs::Odometry>("odom_data_euler", 100);
  
   // Publisher of full odom message where orientation is quaternion
-  odom_data_pub_quat = node.advertise<nav_msgs::Odometry>("odom", 100);
+  odom_data_pub_quat = node.advertise<nav_msgs::Odometry>("/odom", 50);
  
   ros::Rate loop_rate(30); 
      
   while(ros::ok()) {
      
     //if(initialPoseRecieved) {
+
       update_odom();
       publish_quat();
     //}
